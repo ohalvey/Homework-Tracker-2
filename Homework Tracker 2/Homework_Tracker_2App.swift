a02df4d6f3f8e0d153ed99522971ff7160f66431
oliver halvey <oliverhalvey310@yahoo.com>
2022-12-16 22:04:05 -0800

    Initial Commit

---
+++ Homework Tracker 2/Homework_Tracker_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_Tracker_2App.swift
+//  Homework Tracker 2
+//
+//  Created by Oliver Halvey on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_Tracker_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
